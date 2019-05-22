137eab13ed5babca782c57992a18e612c35de43e
Phatthanakorn Uraiwong <peatzahot@hotmail.com>
2019-05-22 20:12:58 +0700

    part edit form

--- backend/MachineMaintenance/src/routes/partsEditForm.php
+++ backend/MachineMaintenance/src/routes/partsEditForm.php
@@ -30,27 +30,68 @@ $app->post("/api/partsEdit/submit", function(Request $request, Response $respons
     $partsID = $request->getParsedBody()['partsId'];
     $partsName = $request->getParsedBody()['partsName'];
     $pricePerUnit = $request->getParsedBody()['pricePerUnit'];
+    $amount = $request->getParsedBody()['amount'];
     $useInModel = $request->getParsedBody()['useInModel'];
+    $modelCode = json_decode($useInModel);
+
+    $sqlParts = "INSERT INTO parts (partsID, partsName, pricePerUnit, amount) VALUES (:partsID, :partsName, :pricePerUnit, :amount);";
+    $sqlPartsUpdate = "UPDATE parts SET partsName = :partsName, pricePerUnit = :pricePerUnit, amount = :amount WHERE partsID = :partsID";
+    $sqlPartsDelete = "DELETE FROM partscompatible WHERE partsID = :partsID;";
+
+    $sqlPartsCompat = "";
     
+
     try {
         $db = new db();
         $db = $db->connect();
 
         if($actionType == 0){
             //INSERT
-            
+
+            foreach($modelCode as $idx => $item){
+                $sqlPartsCompat .= "INSERT INTO partscompatible (partsID, modelCode) VALUES (:partsID, '".$item."');";
+            }
+
+            $stmt = $db->prepare($sqlParts);
+            $stmt->bindParam(':partsID', $partsID, PDO::PARAM_STR);
+            $stmt->bindParam(':partsName', $partsName, PDO::PARAM_STR);
+            $stmt->bindParam(':pricePerUnit', $pricePerUnit, PDO::PARAM_STR);
+            $stmt->bindParam(':amount', $amount, PDO::PARAM_INT);
+
+            $stmtA = $db->prepare($sqlPartsCompat);
+            $stmtA->bindParam(':partsID', $partsID, PDO::PARAM_STR);
+
         }
         else if($actionType == 1){
             //UPDATE
-            
+            $sqlPartsCompat .= "DELETE FROM partscompatible WHERE partsID = :partsID;";
+            foreach($modelCode as $idx => $item){
+                $sqlPartsCompat .= "INSERT INTO partscompatible (partsID, modelCode) VALUES (:partsID, '".$item."');";
+            }
+
+            $stmt = $db->prepare($sqlPartsUpdate);
+            $stmt->bindParam(':partsID', $partsID, PDO::PARAM_STR);
+            $stmt->bindParam(':partsName', $partsName, PDO::PARAM_STR);
+            $stmt->bindParam(':pricePerUnit', $pricePerUnit, PDO::PARAM_STR);
+            $stmt->bindParam(':amount', $amount, PDO::PARAM_INT);
+
+            $stmtA = $db->prepare($sqlPartsCompat);
+            $stmtA->bindParam(':partsID', $partsID, PDO::PARAM_STR);
         }
         else{
             //DELETE
+            $sqlPartsCompat .= "DELETE FROM parts WHERE partsID = :partsID;";
             
+            $stmt = $db->prepare($sqlPartsDelete);
+            $stmt->bindParam(':partsID', $partsID, PDO::PARAM_STR);
+
+            $stmtA = $db->prepare($sqlPartsCompat);
+            $stmtA->bindParam(':partsID', $partsID, PDO::PARAM_STR);
         }
 
         //execute SQL statement and get result
         $result = $stmt->execute();
+        $result .= $stmtA->execute();
         $db = null;
 
         //return result to frontend
