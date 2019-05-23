c15b90b1ad6be8779d782acd509d1e725b231436
Sirawit7205 <lsirawit24@gmail.com>
2019-05-23 07:20:27 +0700

    Job Assignment form

--- backend/MachineMaintenance/src/routes/assignmentForm.php
+++ backend/MachineMaintenance/src/routes/assignmentForm.php
@@ -4,7 +4,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get("/api/job/getCurrentIds", function(Request $request, Response $response) {
-  $sql = "SELECT jobID AS jobId FROM job ORDER BY jobId";
+  $sql = "SELECT jobID AS jobId FROM job WHERE jobStatus != \"Finished\" ORDER BY jobId";
   try {
     $db = new db();
     $db = $db->connect();
@@ -12,6 +12,33 @@ $app->get("/api/job/getCurrentIds", function(Request $request, Response $respons
     $stmt = $db->query($sql);
     $data = $stmt->fetchAll(PDO::FETCH_OBJ);
     $db = null;
+
+    echo json_encode($data);
+  } catch(PDOException $e) {
+    echo '{"error":{"text": '.$e->getMessage().'}}';
+  }
+});
+
+$app->get("/api/job/all/{jobId}", function(Request $request, Response $response) {
+  $jobId = $request->getAttribute('jobId');
+  $sql = "SELECT jobType, date, endDate, startTime, endTime, estimateDuration, machineID AS machineId, details, priority, severity, jobStatus FROM job WHERE jobID = \"$jobId\"";
+  $sqlCurrentStaffList = "SELECT staffID AS staffId FROM assignment WHERE jobID = \"$jobId\"";
+  $sqlAllStaffList = "SELECT staffID AS staffId, staffName FROM staff";
+
+  try {
+    $db = new db();
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+    $stmtS = $db->query($sqlCurrentStaffList);
+    $stmtAS = $db->query($sqlAllStaffList);
+    $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $dataS = $stmtS->fetchAll(PDO::FETCH_OBJ);
+    $dataAS = $stmtAS->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    
+    @$data[0]->staffList = $dataS;
+    @$data[0]->allStaffList = $dataAS;
 
     echo json_encode($data);
   } catch(PDOException $e) {
@@ -34,13 +61,16 @@ $app->post("/api/job/submit", function(Request $request, Response $response) {
     $severity = $request->getParsedBody()['severity'];
     $jobStatus = $request->getParsedBody()['jobStatus']; //job status
     $status = $request->getParsedBody()['status']; //machine status
+    $staff =  $request->getParsedBody()['staff'];
 
+    $staffId = json_decode(json_encode($staff), true);
 
+    $sqlUpdateAssign = "DELETE FROM assignment WHERE jobID=:jobId;";
     $sqlInsert = "INSERT INTO job(jobID,jobType,date,startTime,endDate,endTime,estimateDuration,machineID,details,priority,severity,jobStatus) VALUES(:jobId,:jobType,:date,:startTime,:endDate,:endTime,:estimateDuration,:machineId,:details,:priority,:severity,:jobStatus);
     UPDATE machine SET status=:status WHERE machineID = :machineId;";
     $sqlUpdate = "UPDATE job SET jobType=:jobType,date=:date,startTime=:startTime,endDate=:endDate,endTime=:endTime,estimateDuration=:estimateDuration,machineID=:machineId,details=:details,priority=:priority,severity=:severity,jobStatus=:jobStatus WHERE jobID = :jobId;
     UPDATE machine SET status=:status WHERE machineID = :machineId;";
-    $sqlDelete = "DELETE FROM job WHERE jobID = :jobId";
+    $sqlDelete = "DELETE FROM assignment WHERE jobID = :jobId; DELETE FROM job WHERE jobID = :jobId;";
     
     try {
       $db = new db();
@@ -72,6 +102,13 @@ $app->post("/api/job/submit", function(Request $request, Response $response) {
       else if($actionType == 1) {
         //prepare template
         $stmt = $db->prepare($sqlUpdate);
+
+        //batch insert
+        foreach($staffId as $idx => $item){
+          $sqlUpdateAssign .= "INSERT INTO assignment(jobID,staffID) VALUES(:jobId,'".$item."');";
+        }
+
+        $stmtAssign = $db->prepare($sqlUpdateAssign);
   
         //bind parameters
         $stmt->bindParam(':jobId', $jobId, PDO::PARAM_STR);
@@ -87,6 +124,12 @@ $app->post("/api/job/submit", function(Request $request, Response $response) {
         $stmt->bindParam(':severity', $severity, PDO::PARAM_INT);
         $stmt->bindParam(':jobStatus', $jobStatus, PDO::PARAM_STR);
         $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+
+        $stmtAssign->bindParam(':jobId', $jobId, PDO::PARAM_STR);
+        $resultAssign = $stmtAssign->execute();
+        $stmtAssign->closeCursor();
+
+        echo $resultAssign;
     }
       //DELETE
       else {
